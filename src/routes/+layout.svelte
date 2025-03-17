8ac6d73ec7ebe7a01c51f2099cf6a19cb4c54d92
Kumiko Haraguchi <haraguchi935@gmail.com>
2025-03-17 12:49:11 -0600

    Adjust layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,7 @@
   import '../styles/global.css';
 </script>
 
-<svelte:head>
-  <title>Global Active Archive of Large Flood Events</title>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" crossorigin="anonymous">
-</svelte:head>
+<slot></slot>
 
-<slot></slot> 
+<style>
+</style> 
